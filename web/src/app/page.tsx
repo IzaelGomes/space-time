e37ba3c1f783e5f9600c88176e976a362e9b15e1
izael gomes da silva <izaelgomes127@gmail.com>
2023-05-21 16:44:59 -0300

    chore:spliting main page into components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,8 @@
-import { Copyright, User } from "lucide-react";
-import Image from "next/image";
-import nlwLogo from "../assets/space-time-logo.svg";
+import { Copyright } from "@/components/Copyright";
+import { EmptyMemories } from "@/components/EmptyMemories";
+import { Hero } from "@/components/Hero";
+import { Signin } from "@/components/Signin";
+import { User } from "lucide-react";
 
 export default function Home() {
   return (
@@ -12,52 +14,16 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes" />
 
         {/*Sign in*/}
-
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>{" "}
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
+        <Signin />
 
         {/*hero*/}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="" />
-          <div className="max-w-[420px] space-y-1">
-            <h1 className=" text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p>
-              Colecione momentos marcantes da sua jornada e compartilhe(se
-              quiser) com o mundo
-            </p>
-          </div>
-          <a
-            className=" inline-block rounded-full bg-green-500  px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-            href="#"
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
+        <Hero />
         <Copyright />
       </div>
 
       {/*Right*/}
       <div className="flex flex-col bg-gray-900 bg-[url('../assets/bg-stars.svg')] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{" "}
-            <a href="#" className="underline hover:text-gray-50">
-              criar agora!
-            </a>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   );
